05c3daa6c5debb4bbaf795839959e62f8de300d4
EC2 Default User <ec2-user@ip-172-31-21-120.us-east-2.compute.internal>
2018-12-06 07:53:52 +0000

    dynamic links added to dashboard

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,10 +1,28 @@
 class PodcastsController < ApplicationController
+    before_action :find_podcast, only: [:show, :dashboard]
+    before_action :find_episode, only: [:show, :dashboard]
+    
     def index
         @podcasts = Podcast.all.order("created_at DESC") # defining the @podcasts method, loop through all the podcasts and order by created at in descending order
     end
     
     def show # defining the action show
-        @podcast = Podcast.find(params[:id]) # Find the podcast by the parameters of id
+    end
+    
+    def dashboard
+    end
+    
+    private
+    
+    def find_episode
         @episodes = Episode.where(podcast_id: @podcast).order("created_at DESC") # find the episode by podcast ID
+    end
+    
+    def find_podcast
+        if params[:id].nil? # conditional statement if rails cannot find a podcast(user) without an id
+            @podcast = current_podcast
+        else
+        @podcast = Podcast.find(params[:id]) # Find the podcast by the parameters of id
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
     
   devise_for :podcasts
-    root 'welcome#index' # makes the root of the application go to the welcome controller
     
     resources :podcasts, only: [:index, :show] do #creates resources for our podcasts
       resources :episodes #resources for episodes, nested in the podcasts route
-      end 
+      end
+      
+      authenticated :podcast do # devise method that roots the application to the podcast dashboard if the user is signed in
+        root 'podcasts#dashboard', as: "authenticated_root"
+      end
+      
+      root 'welcome#index' # makes the root of the application go to the welcome controller if a user is not signed in
 end
