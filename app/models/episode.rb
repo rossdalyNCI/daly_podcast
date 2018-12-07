d4fc79d860ae077bba5f18270c41f3b1c226053b
EC2 Default User <ec2-user@ip-172-31-21-120.us-east-2.compute.internal>
2018-12-07 07:19:34 +0000

    Added paperclip for MP3 uploading

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,4 +3,7 @@ class Episode < ApplicationRecord
   
   has_attached_file :episode_thumbnail, styles: { large: "1000x1000#", medium: "550x550#" } # needed for paperlcip image uploading
   validates_attachment_content_type :episode_thumbnail, content_type: /\Aimage\/.*\z/
+  
+  has_attached_file :mp3 # needed for mp3 uploading
+  validates_attachment :mp3, :content_type => { :content_type => ["audio/mpeg", "audio/mp3"]}, :file_name => { :matches => [/mp3\Z/]} # validates the attachment and makes sure the file matches .mp3
 end
